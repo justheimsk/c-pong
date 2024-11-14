69039c2ae0bafe7bf82836e9e18ab75a42e55ec4
Henrique <henrique.mdimer@gmail.com>
2024-11-14 17:44:20 -0300

    add player collision

--- src/ball.c
+++ src/ball.c
@@ -3,6 +3,17 @@
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 
+int check_player_colision(Ball* ball, Player* player) {
+  if(ball->pos->x < player->pos->x + player->w &&
+      ball->pos->x + ball->w > player->pos->x &&
+      ball->pos->y < player->pos->y + player->h &&
+      ball->pos->y + ball->h > player->pos->y) {
+    return 1;
+  }
+
+  return 0;
+}
+
 Ball* ball_create(float x, float y) 
 {
   Vec2 *pos = vec2_create(x, y);
@@ -23,13 +34,15 @@ void ball_render(SDL_Renderer *renderer, Ball *ball)
   SDL_RenderFillRect(renderer, &(SDL_Rect){ .x=ball->pos->x, .y=ball->pos->y, .w=ball->w, .h=ball->h });
 }
 
-void ball_upadate(Ball *ball)
+void ball_upadate(Ball *ball, Player* player1, Player* player2)
 {
   ball->pos->y += ball->vel->y;
   ball->pos->x += ball->vel->x;
 
   if(ball->pos->x <= 0 || ball->pos->x >= WINDOW_W - ball->w) ball->vel->x *= -1;
   if(ball->pos->y <= 0 || ball->pos->y >= WINDOW_H - ball->h) ball->vel->y *= -1;
+
+  if(check_player_colision(ball, player1) || check_player_colision(ball, player2)) ball->vel->x *= -1; 
 }
 
 void ball_destroy(Ball *ball)
